02e695e2f718f2852187983765d8c8b1cfd9fb52
Waqas Kiffal <waqas.kiffal@tenpearls.com>
2018-02-14 16:29:49 +0500

    Add TripleDES OFB, ECB and CFB
    Signed-off-by: Waqas Kiffal <waqas.kiffal@tenpearls.com>

--- aes-encdec/main.c
+++ aes-encdec/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include <openssl/ssl.h>
+#include <string.h>
+#include <openssl/evp.h>
 #include <openssl/err.h>
 
 void initialize_fips(int mode) {
@@ -14,78 +15,160 @@ void initialize_fips(int mode) {
     }
 }
 
-void main(int argc, char **argv) {
+void print_hex(FILE *out, const char *s) {
+  while(*s)
+    fprintf(out, "%02x", (unsigned char) *s++);
+    fprintf(out, "\n");
+}
+
+int str2hex(const char hexstring[], unsigned char * val, int *len) {
+    const char *pos = hexstring;
+    size_t count = 0;
+
+    if (strlen(hexstring) % 2 == 1) return 0;
+
+     /* WARNING: no sanitization or error-checking whatsoever */
+    for (count = 0; count < strlen(hexstring)/2; count++) {
+        sscanf(pos, "%2hhx", &val[count]);
+        pos += 2;
+    }
+    *len = strlen(hexstring) / 2;
+
+    return 1;
+}
+
+void main(int argc, char *argv[])
+{
     initialize_fips(1);
 
-    unsigned char *key = (unsigned char *)"example key 1234";
-    unsigned char *iv = (unsigned char *)"0123456789012341";
-    unsigned char *plaintext = (unsigned char *)"exampleplaintext";
+    unsigned char *key_str = (unsigned char *)"example key 1234example key 1234";
+    unsigned char *iv_str = (unsigned char *)"0123456789012341";
+    unsigned char *plaintext = (unsigned char *)"exampleplaintextexampleplaintext";
     unsigned char *mode = (unsigned char *)"cbc";
+    unsigned char key[1024];
+    unsigned char iv[1024];
+    int key_len = 0, iv_len = 0;
+    unsigned char ciphertext[1024];
+    unsigned char decryptedtext[1024];
+    int decryptedtext_len = 0, ciphertext_len = 0;
 
-    if (argc == 2) {
-        mode = argv[1];
+    if (argc >= 3) {
+      key_str = argv[1];
+      iv_str  = argv[2];
+
+      if (argc >= 4) {
+        plaintext = argv[3];
+      }
+
+      if (argc >= 5) {
+        mode = argv[4];
+      }
+    } else {
+        fprintf(stderr, "USAGE: %s [key] [iv] [plain-text] [mode]\nBy default mode is 'cbc'\n", argv[0]);
+        return 1;
     }
 
-    printf(mode);
-
-    // Encryption
-    fprintf(stdout, "\nEncryption:\n");
-    unsigned char ciphertext[32];
-    int ciphertext_len;
-    ciphertext_len = encdec(plaintext, strlen((char *)plaintext), key, strlen((char *)key), iv, ciphertext, mode, 1);
-    if (ciphertext_len == 0) {
+    /* Convert key and iv from string to hex */
+    if (!str2hex(key_str, key, &key_len)) {
+        printf("ERROR");
         return;
     }
 
+    if (!str2hex(iv_str, iv, &iv_len)) {
+        printf("ERROR");
+        return;
+    }
+
+    /* Encrypt the plaintext */
+    fprintf(stdout, "\nEncryption:\n");
+
+    ciphertext_len = encdec(plaintext, strlen(plaintext), key, key_len, iv, ciphertext, mode, 1);
+
+    /* Do something useful with the ciphertext here */
     fprintf(stdout, "Plaintext: %s\n", plaintext);
+    fprintf(stdout, "KEY: ");
+    print_hex(stdout, key);
     fprintf(stdout, "IV: ");
     print_hex(stdout, iv);
-    fprintf(stdout, "Ciphertext [%d]: ", ciphertext_len);
+    fprintf(stdout, "Ciphertext : ");
     print_hex(stdout, ciphertext);
 
-    // Decryption
+    /* Decrypt the ciphertext */
     fprintf(stdout, "\nDecryption:\n");
-    unsigned char pt_tmp[32];
-    int pt_len;
-    pt_len = encdec(ciphertext, strlen((char *)ciphertext), key, strlen((char *)key), iv, pt_tmp, mode, 0);
-    if (pt_len == 0) {
-        return;
+
+    decryptedtext_len = encdec(ciphertext, ciphertext_len, key, key_len, iv, decryptedtext, mode, 0);
+
+    if(decryptedtext_len < 0)
+    {
+        /* Verify error */
+        printf("Decrypted text failed to verify\n");
     }
-    // Print decrypted
-    fprintf(stdout, "Plaintext [%d]: %s\n", pt_len, pt_tmp);
+    else
+    {
+        /* Add a NULL terminator. We are expecting printable text */
+        decryptedtext[decryptedtext_len] = '\0';
+
+        /* Show the decrypted text */
+        printf("Decrypted text is: ");
+        printf("%s\n", decryptedtext);
+    }
+
+    /* Remove error strings */
+    ERR_free_strings();
+
 }
 
-void print_hex(FILE *out, const char *s)
-{
-  while(*s)
-    fprintf(out, "%x", (unsigned char) *s++);
-  fprintf(out, "\n");
-}
+int encdec(unsigned char *plaintext, int plaintext_len, unsigned char *key, int key_len,
+            unsigned char *iv, unsigned char *ciphertext, unsigned char *mode, int enc) {
 
-int encdec(unsigned char *plaintext, int plaintext_len, unsigned char *key, int key_len, unsigned char *iv, unsigned char *ciphertext, unsigned char *mode, int enc) {
-    // New CBC Encrypter
-    EVP_CIPHER *evpCipher;
+    EVP_CIPHER_CTX *ctx = NULL;
+    int len = 0, ciphertext_len = 0;
+
+    /* Create and initialise the context */
+    ctx = malloc(sizeof(EVP_CIPHER_CTX));
+    FIPS_cipher_ctx_init(ctx);
+
+    /* Initialise the encryption operation. */
+    const EVP_CIPHER *evpCipher;
     switch (key_len) {
     case 16:
         if (strcmp(mode,"cbc")==0) {
-            evpCipher = EVP_aes_128_cbc();
-        }else{
+            evpCipher = FIPS_evp_aes_128_cbc();
+        } else if (strcmp(mode,"ctr")==0) {
+            evpCipher = FIPS_evp_aes_128_ctr();
+        } else if (strcmp(mode,"gcm")==0) {
+            evpCipher = FIPS_evp_aes_128_gcm();
+        } else if (strcmp(mode,"ecb")==0) {
+            evpCipher = FIPS_evp_aes_128_ecb();
+        } else {
             fprintf(stderr, "invalid mode\n");
             return 0;
         }
         break;
     case 24:
         if (strcmp(mode,"cbc")==0) {
-            evpCipher = EVP_aes_192_cbc();
-        }else{
+            evpCipher = FIPS_evp_aes_192_cbc();
+        } else if (strcmp(mode,"ctr")==0) {
+            evpCipher = FIPS_evp_aes_192_ctr();
+        } else if (strcmp(mode,"gcm")==0) {
+            evpCipher = FIPS_evp_aes_192_gcm();
+        } else if (strcmp(mode,"ecb")==0) {
+            evpCipher = FIPS_evp_aes_192_ecb();
+        } else {
             fprintf(stderr, "invalid mode\n");
             return 0;
         }
         break;
     case 32:
         if (strcmp(mode,"cbc")==0) {
-            evpCipher = EVP_aes_256_cbc();
-        }else{
+            evpCipher = FIPS_evp_aes_256_cbc();
+        } else if (strcmp(mode,"ctr")==0) {
+            evpCipher = FIPS_evp_aes_256_ctr();
+        } else if (strcmp(mode,"gcm")==0) {
+            evpCipher = FIPS_evp_aes_256_gcm();
+        } else if (strcmp(mode,"ecb")==0) {
+            evpCipher = FIPS_evp_aes_256_ecb();
+        } else {
             fprintf(stderr, "invalid mode\n");
             return 0;
         }
@@ -95,36 +178,41 @@ int encdec(unsigned char *plaintext, int plaintext_len, unsigned char *key, int 
         return 0;
     }
 
-    EVP_CIPHER_CTX *ctx = malloc(sizeof(EVP_CIPHER_CTX));
-    EVP_CIPHER_CTX_init(ctx);
-    if (EVP_CipherInit_ex(ctx, evpCipher, NULL, NULL, NULL, enc) <= 0) {
-        fprintf(stderr, "EVP_CipherInit_ex failed (1)\n");
-        return 0;
+    if(FIPS_cipherinit(ctx, evpCipher, NULL, NULL, enc) <= 0) {
+      fprintf(stderr, "FIPS_cipherinit failed (1)\n");
+      return 0;
     }
 
-    if (EVP_CipherInit_ex(ctx, NULL, NULL, key, iv, enc) <= 0) {
-        fprintf(stderr, "EVP_CipherInit_ex failed (2)\n");
-        return 0;
+    /* Set IV length if default 12 bytes (96 bits) is not appropriate for GCM mode */
+    if(strcmp(mode,"gcm")==0)
+        if(1 != FIPS_cipher_ctx_ctrl(ctx, EVP_CTRL_GCM_SET_IVLEN, strlen(iv), NULL))
+            fprintf(stderr, "FIPS_cipher_ctx_ctrl GCM_SET_IVLEN failed. \n");
+
+    /* Initialise key and IV */
+    if(FIPS_cipherinit(ctx, NULL, key, iv, enc) <= 0) {
+      fprintf(stderr, "FIPS_cipherinit failed (2)\n");
+      return 0;
     }
 
-    EVP_CIPHER_CTX_set_padding(ctx, 0);
+    EVP_CIPHER_CTX_set_padding(ctx, plaintext_len % key_len);
 
-    // Crypt Block
-    if (plaintext_len % key_len != 0) {
-        fprintf(stderr, "plaintext is not a multiple of blocksize\n");
-        return 0;
+    /* Provide the message to be encrypted, and obtain the encrypted output. */
+    if(plaintext) {
+        if(1 != EVP_CipherUpdate(ctx, ciphertext, &len, plaintext, plaintext_len)) {
+          fprintf(stderr, "EVP_CipherUpdate failed \n");
+        }
+
+        ciphertext_len = len;
     }
 
-    // crypt
-    int outLen;
-    if (EVP_CipherUpdate(ctx,
-        	&ciphertext[0], &outLen,
-			&plaintext[0], plaintext_len) != 1) {
-			return 0;
+    /* Finalise the cryption. Normally ciphertext bytes may be written at this stage */
+    if(1 != EVP_CipherFinal_ex(ctx, ciphertext + len, &len)) {
+      fprintf(stderr, "EVP_CipherFinal_ex failed \n");
     }
+    ciphertext_len += len;
 
-    if (plaintext_len != outLen) {
-        fprintf(stderr, "openssl did not encryt all of plaintext %d, %d\n", plaintext_len, outLen);
-        return 0;
-    }
+    /* Clean up */
+    FIPS_cipher_ctx_free(ctx);
+
+    return ciphertext_len;
 }
